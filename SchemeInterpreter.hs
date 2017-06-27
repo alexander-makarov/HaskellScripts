8e7ffde71a481885ebbb319c223639ebdae0f005
Alexander Makarov <makarov.dev@gmail.com>
2017-06-27 22:31:47 +0200

    lists and quoted

--- SchemeInterpreter.hs
+++ SchemeInterpreter.hs
@@ -28,19 +28,10 @@ data LispVal = Atom String
 --In general, use >> if the actions don't return a value, >>= if you'll be immediately passing that value into the next action, and do-notation otherwise.
 parseString :: Parser LispVal
 parseString = do
-                --char '"'
-                x <- many ((noneOf "\"\r") <|> (parseEscapedQuote) <|> (parseEscapedSlash))
-                --x <- many (char '"')
-                --char '"'
+                char '"'
+                x <- many ((noneOf "\"")
+                char '"'
                 return $ String x
-                
-parseEscapedQuote = do
-               string "\""
-               return '"'
-
-parseEscapedSlash = do
-               string "\r"
-               return '0'
                
 parseNumber :: Parser LispVal
 -- parseNumber = liftM (Number . read) $ many1 digit
@@ -64,10 +55,27 @@ parseAtom = do
 parseList :: Parser LispVal
 parseList = liftM List $ sepBy parseExpr spaces
 
+parseDottedList :: Parser LispVal
+parseDottedList = do
+    head <- endBy parseExpr spaces
+    tail <- char '.' >> spaces >> parseExpr
+    return $ DottedList head tail
+    
+parseQuoted :: Parser LispVal
+parseQuoted = do
+    char '\''
+    x <- parseExpr
+    return $ List [Atom "quote", x]
+
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
          <|> parseString
          <|> parseNumber
+         <|> parseQuoted
+         <|> do char '('
+                x <- try parseList <|> parseDottedList
+                char ')'
+                return x
 
 main :: IO ()
 main = do 
